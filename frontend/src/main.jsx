ecf4cc2ecd469b629a6ce31c2d5a75eed3bb9188
amjad36942 <57553785+amjad36942@users.noreply.github.com>
2026-04-13 10:07:24 +0500

    Fix JSX syntax in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter basename="/pdf-toolkit">
-              <App />
-        </BrowserRouter>BrowserRouter>
-    </React.StrictMode>React.StrictMode>
-  )</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/pdf-toolkit">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
